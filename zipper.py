fe6dfe2bd13beccec05f02729bbc2ea2478d4741
George Chang <59218296+gchang12@users.noreply.github.com>
2025-02-09 10:31:39 -0800

    Refactored some stuff.

--- zipper.py
+++ zipper.py
@@ -7,8 +7,9 @@ from zipfile import ZipFile
 import logging
 import os.path
 import os
-import sys
+import argparse
 from pathlib import Path
+import shutil
 
 #zipfile = ZipFile()
 #help(ZipFile)
@@ -27,19 +28,18 @@ def unzip(filename):
     Extracts contents of epub file into a directory in OUTPUTDIR_NAME.
     """
     zipfile = ZipFile(filename)
-    subdir = filename.replace('.epub', '') \
-        .replace(QUEUEDIR_NAME, '')
-    path = '/'.join([OUTPUTDIR_NAME, subdir]).replace('//', '/')
+    path = str(Path(OUTPUTDIR_NAME, Path(filename).with_suffix("").name))
     logging.debug(
-        "Extracting epub contents into %r",
-        path,
+        "Extracting epub contents into %r", path,
     )
     zipfile.extractall(path=path)
+    logging.debug("Currently in directory: '%s'", os.getcwd())
 
 def edit_file(dirname):
     """
     Removes problematic ampersands from book.* files.
     """
+    logging.debug("Currently in directory: '%s'", os.getcwd())
     #os.system("vim %s/book.*" % dirname)
     #ampersand_replacement = "&#038;"
     ampersand_replacement = "&amp;#038;"
@@ -60,26 +60,44 @@ def rezip(dirname):
     """
     # https://www.reddit.com/r/vim/comments/gwcqd9/comment/fsuflnb/
     os.chdir(dirname)
-    epub_name = dirname.replace(OUTPUTDIR_NAME, "") \
-        .strip('/') \
-        + ".epub"
-    os.system("zip -X0 %r mimetype 2>/dev/null" % epub_name)
-    os.system("zip -Xur9D %r * 2>/dev/null" % epub_name)
-    os.system("mv %r ../" % epub_name)
-    logging.debug(
-        "Fixed epub file is in output/%s",
-        epub_name,
-    )
+    logging.debug("Currently in directory: '%s'", os.getcwd())
+    epub_name = Path(dirname).with_suffix(".epub").name
+    logging.debug("epub name: '%s'", epub_name)
+    os.system("zip -X0 '%s' mimetype >/dev/null" % epub_name)
+    os.system("zip -Xur9D '%s' * >/dev/null" % epub_name)
+    os.system("mv '%s' ../" % epub_name)
+    return epub_name
 
-filename = sys.argv[1]
+def cleanup(dirname):
+    """
+    Deletes unzipped directory.
+    """
+    #logging.debug("Invoking 'shutil.rmtree' on '%s'", dirname)
+    os.chdir("../..")
+    logging.debug("Currently in directory: '%s'", os.getcwd())
+    shutil.rmtree(dirname)
+
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("filename")
+parser.add_argument("--delzip", type=bool, default=True)
+args = parser.parse_args()
+filename = args.filename
+delzip = args.delzip
+
+# main
+logging.debug("Unzipping '%s'", filename)
 unzip(filename)
-dirname = OUTPUTDIR_NAME \
-    + "/" \
-    + filename.split('/')[-1] \
-    .replace('.epub', '/') \
-    .replace("//", "/")
-# edit
+dirname = Path(OUTPUTDIR_NAME, Path(filename).with_suffix("").name)
+logging.debug("Editing unzipped directory: '%s'", dirname)
 edit_file(dirname)
-# rezip
-rezip(dirname)
+logging.debug("Rezipping folder: '%s'", dirname)
+epub_name = rezip(dirname)
+if delzip:
+    logging.debug("Deleting folder: '%s'", dirname)
+    cleanup(dirname)
+    logging.debug("Deletion successful.")
+logging.debug(
+    "Fixed epub file is in 'output/%s'", epub_name,
+)
 
